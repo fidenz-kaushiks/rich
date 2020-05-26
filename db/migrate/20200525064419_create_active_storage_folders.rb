edf2789d9b962985023fa0acf876422ebdfc254b
kaushik shamantha <kaushik.s@fidenz.com>
2020-05-26 10:13:23 +0530

    reset to default

--- db/migrate/20200525064419_create_active_storage_folders.rb
+++
@@ -1,9 +0,0 @@
-class ActiveStorageFolders < ActiveRecord::Migration[5.2]
-  def change
-    create_table :active_storage_folders do |t|     
-      t.string :folder_name
-      t.integer :parent
-      t.timestamps
-    end
-  end
-end
