f7df0a2f91f93a37bf53c46a46277d8d1710324b
kaushik shamantha <kaushik.s@fidenz.com>
2020-06-24 17:18:02 +0530

    hide rich style selector and migration clean

--- db/migrate/20200527110044_create_active_storage_folder_tables.rb
+++ db/migrate/20200527110044_create_active_storage_folder_tables.rb
@@ -6,11 +6,5 @@ class CreateActiveStorageFolderTables < ActiveRecord::Migration[5.2]
       t.datetime :created_at, null: false
       t.datetime :updated_at, null: false
     end
-
-    create_table :active_storage_folder_attachments do |t|
-      t.integer :active_storage_folder_id
-      t.integer :active_storage_attachment_id
-      t.datetime :created_at, null: false
-    end
   end
 end
